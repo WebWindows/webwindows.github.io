032b35b08cda219a65b624c31012396c075a83a8
tcpsoftware <1691957831@qq.com>
2019-02-03 15:52:25 +0800

    commit 2018-2-24

--- dust_bin/bin.js
+++ dust_bin/bin.js
@@ -27,3 +27,61 @@
 	// $("#dragme").draggable({
 	// 	containment: "parent"
 	// });
+	
+//			var tmp_icon_pos = "(" + tmp_hang_pos + "," + tmp_lie_pos + ")";
+//			console.log("icon_pos:" + tmp_icon_pos + ".the_end");
+//			if($(this).parent().attr("id") != "innerdesktop"){
+//				$(this).unwrap();
+//			}
+//			var temp=new blockDrag({
+//				boxNode: "#innerdesktop",
+//				dragNode: "#" + the_id,
+//				blockW: 75,
+//				blockH: 100,
+//				block: cnt_of_all_block,
+//				startPos: tmp_icon_pos
+//			});
+
+//		tmp_element.attr('tabindex',"-1");
+//		tmp_element.bind('click', function () { this.focus(); });
+			// console.log(tmp_element.html() );
+			
+
+//	$("#innerdesktop").contextMenu("desktop_menu_normal",
+//	{
+//		//菜单样式
+//		menuStyle: {
+//			border: "1px solid darkgray",
+//			padding: "4px 2px",
+//			width: "200px"
+//		},
+//		//菜单项样式
+//		itemStyle: {
+//			fontFamily: 'Microsoft Yahei',
+//			backgroundColor: "white",
+//			color: "black",
+//			border: 'none',
+//			padding: "2px 5px",
+//			height: "22px"
+//
+//		},
+//		//菜单项鼠标放在上面样式
+//		itemHoverStyle: {
+//			color: 'blue',
+//			backgroundColor: 'red',
+//			border: 'none'
+//		},
+//		bindings: {'open': function(t) {
+//				console.log(t);
+//			},
+//			'email': function(t) {
+//				console.log(t);
+//			},
+//			'save': function(t) {
+//				console.log(t);
+//			},
+//			'delete': function(t) {
+//				console.log(t);
+//			}
+//		}
+//	});
