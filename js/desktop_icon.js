59fef452dcbc6fce93174dc20bdb0264a64bbb04
tcpsoftware <1691957831@qq.com>
2019-02-03 16:02:29 +0800

    commit 2018-02-25

--- js/desktop_icon.js
+++ js/desktop_icon.js
@@ -29,7 +29,7 @@
 		});
 		$(".the_focus").removeClass("the_focus");
 	}
-	function add_desktop_shortcut(shortcut_name,img_src){
+	function add_desktop_shortcut(shortcut_name , img_src, window_name, window_icon_src, title_name, app_location, iframe_style){
 		var tmp_element = $("<div>");
 		tmp_element.addClass("desktop_icon");
 		tmp_element.append('<div><img/><span></span></div>');
@@ -80,6 +80,32 @@
 						}
 					})
 					.click(function() {$(this).children().first().addClass("ui-selected");$(".the_focus").removeClass("the_focus");$(this).addClass("the_focus");})
+					.dblclick(function(){
+						console.log("once dblclick");
+						var tmp_string = 
+	'<div class="desktop_window" style="display: none;">' +
+		'<div class="window_title_bar" >' +
+			'<img  class="window_icon" draggable="false" src="app/images/icons/speaker-16x16.png">' +
+			'<span class="window_title"></span>' +
+			'<button class="window_close_button window_button"></button>' +
+		'</div>' +
+		'<div class="window_content">' +
+			'<iframe allowfullscreen=""></iframe>' +
+		'</div>' +
+	'</div>';
+						var tmp_element = $(tmp_string);
+						tmp_element.attr("id",window_name);
+						tmp_element.children().first().children().eq(1).html(title_name);
+						tmp_element.children().first().children().eq(0).attr("src",window_icon_src);
+						tmp_element.children().eq(1).children().first().attr("src",app_location);
+						tmp_element.children().eq(1).children().first().css(iframe_style);
+						tmp_element.children().eq(0).children().eq(2).click(function(){
+							$(this).parent().parent().remove();
+						});
+						$("#innerdesktop").append(tmp_element);
+						tmp_element.draggable({scroll: false, handle:".window_title_bar"});
+						tmp_element.show();
+					})
 					.css("position", "absolute");
 		$("#innerdesktop").append(tmp_element);
 		desktop_refresh();
@@ -107,7 +133,7 @@
 	}
 	$.getJSON("setting/desktop_icons.json",function(json){
 		$.each(json.shortcut,function(){
-			add_desktop_shortcut(this.name , this.imgsrc);
+			add_desktop_shortcut(this.name , this.imgsrc, this.window_name, this.window_icon_src, this.title_name, this.app_location, this.iframe_style);
 		});
 		desktop_refresh();
 	});
