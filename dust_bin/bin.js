a7f1f6c5d3be6a10ca01093f8ae4c71c4c255525
tcpsoftware <1691957831@qq.com>
2019-02-03 15:38:51 +0800

    commit 2018-1-21

---
+++ dust_bin/bin.js
@@ -0,0 +1,29 @@
+	// console.log("window_width " + window_width);
+	// console.log("window_height " + window_height);
+	// console.log("window_height-40= " + (window_height-40));
+	// console.log("desktop_icons_per_lie " + desktop_icons_per_lie);
+	// console.log("desktop_icons_per_hang " + desktop_icons_per_hang);
+    // console.log(cnt_of_all_block + ".the_end");
+    
+	// $(".desktop_icon").draggable({
+	// 	opacity: 0.7,
+	// 	scroll: false,
+	// 	containment: "parent",
+	// 	// snap: true,
+	// 	// snapMode: "outer",
+	// 	// snapTolerance: 30,
+	// 	revert: "valid",
+	// 	start: function(){
+	// 		// return false;
+	// 	},
+	// 	drag: function(){
+	// 		// return false;
+	// 	},
+	// 	end: function(){
+	// 		// return false;
+	// 	}
+	// }).droppable();
+
+	// $("#dragme").draggable({
+	// 	containment: "parent"
+	// });
